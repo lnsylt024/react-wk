857929fb3b908a61c6f99c6a94f883752bf5844a
lnsylt <lnsylt@163.com>
2023-10-19 15:48:17 +0800

    TodoList.jsを追加

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 import './App.css';
+import TodoList from './TodoList';
 
 function App() {
+  //参数[todos]是Object
+  const [todos, setTodos] = useState(["Todo1","Todo2"]);
   return (
-    <div className="App">Hello,World!</div>
+    <div className="App">
+      Hello,World!      
+      <TodoList todoskey={todos}/>
+      <input type="text"/>
+      <button>Add</button>
+      <button>Del</button>
+      <div>
+        count:0
+      </div>
+    </div>
   );
 }
 
---
+++ src/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoList = ({todoskey}) => {
+  return (
+    <>
+    <div>TodoList:{todoskey}</div>    
+    </>
+  )
+}
+
+export default TodoList;
