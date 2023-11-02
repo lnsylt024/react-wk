9b12d8541a140a6ea74ca65b55cd50584896f177
lnsylt <lnsylt@163.com>
2023-11-02 10:59:57 +0800

    fix

--- src/card/index.js
+++ src/card/index.js
@@ -1,17 +1,23 @@
+import {SubCardOne,SubCardTwo,SubCardThree} from "./sub"
 
-
-function Card({activateCard}) {
+function Card({children}) {
     return (
         <div>
-            {activateCard}
+            {children}
         </div>
     );
 }
 
 function Index() {
+    const obj = {
+        "name":"fenghuang",
+        "age":44
+    }
     return (
-        <Card activateCard={"aaaa"}>
-
+        <Card>
+            <SubCardOne name={"litao"}/>
+            <SubCardTwo name={"ABC"}/>
+            <SubCardThree obj={obj}/>
         </Card>
     );
 }
--- src/card/sub/index.js
+++ src/card/sub/index.js
@@ -1,8 +1,24 @@
 export function SubCardOne({name}) {
     return (
-
+        <div>
             <span>Sub One, {name}</span>
-
+        </div>
     );
 }
 
+export function SubCardTwo({name}) {
+    return (
+        <div>
+            <span>Sub Two, {name}</span>
+        </div>
+    );
+}
+
+export function SubCardThree({obj}) {
+    return(
+        <div>
+            <span>{obj.name}</span><br/>
+            <span>{obj.age}</span><br/>
+        </div>
+    );
+}
